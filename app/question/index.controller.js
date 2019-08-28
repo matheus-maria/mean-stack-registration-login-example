93ff6d51722913a7ce11f454258cfae9c267a065
matheus-maria <mmatheus.maria@gmail.com>
2019-08-27 22:28:37 -0300

    Question Tab

---
+++ app/question/index.controller.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Question.IndexController', Controller);
+
+    function Controller($window, UserService, FlashService) {
+        
+    }
+
+})();
